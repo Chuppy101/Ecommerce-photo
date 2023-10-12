5450fe6f32d680d72628717943015c3475e808c4
Chuppy101 <very_vazhno@mail.ru>
2023-10-12 20:41:38 +0300

    Project Finished

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react"
+//import components
+import LatestProducts from "../components/LatestProducts.js"
+import Hero from "../components/Hero.js"
+
+const Home = () => {
+	return (
+		<section>
+			<Hero />
+			<LatestProducts />
+		</section>
+	)
+}
+
+export default Home
